30f6336b8db1ef3458d7f670ea212171390b73a4
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-10 14:45:38 -0400

    add picture for kitten page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
 
+  #add value for main header title
+  def welcome
+    @header = "This is the welcome page"
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/400/200/cats"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome' => 'pages#welcome'
   get '/' => 'pages#welcome'
+
+  #adding link for about us page
   get '/about' => 'pages#about'
+
+  #adding link for contest page
   get '/contest' => 'pages#contest'
+
+  #adding link for kitten page
+  get '/kitten' => 'pages#kitten'
+
+  #add method to pull itten photo by size
+  get '/kitten/:size' => 'pages#kitten'
+
 end
